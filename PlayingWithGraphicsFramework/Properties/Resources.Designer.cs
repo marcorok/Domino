cf6f08e703e67c5cecc93250ec062e073df2d7d5
Marco Roque <marcoroque@microsoft.com>
2023-07-10 15:01:07 +0100

    Removing unnecessary files

--- PlayingWithGraphicsFramework/Properties/Resources.Designer.cs
+++ PlayingWithGraphicsFramework/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PlayingWithGraphicsFramework.Properties {
+namespace DominoClient.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PlayingWithGraphicsFramework.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PlayingWithGraphicsFramework.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DominoClient.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
